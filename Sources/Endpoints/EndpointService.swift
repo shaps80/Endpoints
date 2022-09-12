83efa98ade5cc64349ab1f10278efee0f498cd26
Shaps Benkau <shapsuk@me.com>
2022-09-12 15:10:31 +0100

    Adds observer support

--- Sources/Endpoints/EndpointService.swift
+++ Sources/Endpoints/EndpointService.swift
@@ -1,4 +1,4 @@
-import SwiftUI
+import Foundation
 
 /// Represents a session for an EndpointService
 ///
@@ -14,17 +14,29 @@ public protocol EndpointSession {
 /// Represents a service that can perform requests against `domains` and their associated `endpoints`
 public final class EndpointService {
     let session: EndpointSession
+    private var observers: [ObjectIdentifier: WeakBox] = [:]
 
     /// Creates a new service with the specified session
     /// - Parameter session: The session that will process endpoint requests
-    public init(session: EndpointSession? = nil) {
+    public init(session: EndpointSession? = nil, observers: [EndpointServiceObserver] = [EndpointPrintLogger()]) {
         self.session = session ?? Session()
+        observers.forEach { register($0) }
+    }
+
+    public func register(_ observer: EndpointServiceObserver) {
+        observers[ObjectIdentifier(observer)] = .init(object: observer) { [weak self] in
+            self?.unregister(observer)
+        }
+    }
+
+    public func unregister(_ observer: EndpointServiceObserver) {
+        observers[ObjectIdentifier(observer)] = nil
     }
 }
 
 // MARK: Codable
-public extension EndpointService {
 
+public extension EndpointService {
     /// Performs a request for the specified `endpoint`
     /// - Parameters:
     ///   - endpoint: The `endpoint` configuration
@@ -32,10 +44,11 @@ public extension EndpointService {
     ///   - body: The encodable body to include in the URLRequest
     /// - Returns: The decoded data and response returned for this endpoint request
     func perform<E>(_ endpoint: E, from domain: Domain) async throws -> (E.Output, HTTPURLResponse)
-    where E: DecodableEndpoint & EncodableEndpoint {
-        let encoded = try encode(endpoint: endpoint)
+    where E: DecodableEndpoint & EncodableEndpoint
+    {
+        let encoded = try encode(endpoint: endpoint, for: domain)
         let result = try await perform(endpoint, from: domain, data: encoded)
-        return (try decode(result.0, endpoint: endpoint), result.1)
+        return (try decode(result.0, endpoint: endpoint, for: domain), result.1)
     }
 
     /// Performs a request for the specified `endpoint`
@@ -45,8 +58,9 @@ public extension EndpointService {
     ///   - body: The encodable body to include in the URLRequest
     /// - Returns: The data and response returned for this endpoint request
     func perform<E>(_ endpoint: E, from domain: Domain) async throws -> (E.Output, HTTPURLResponse)
-    where E: DecodableEndpoint & EncodableEndpoint, E.Output == Data {
-        let encoded = try encode(endpoint: endpoint)
+    where E: DecodableEndpoint & EncodableEndpoint, E.Output == Data
+    {
+        let encoded = try encode(endpoint: endpoint, for: domain)
         return try await perform(endpoint, from: domain, data: encoded)
     }
 
@@ -57,9 +71,10 @@ public extension EndpointService {
     ///   - body: The body data to include in the URLRequest
     /// - Returns: The decoded data and response returned for this endpoint request
     func perform<E>(_ endpoint: E, from domain: Domain) async throws -> (E.Output, HTTPURLResponse)
-    where E: DecodableEndpoint & EncodableEndpoint, E.Input == Data {
+    where E: DecodableEndpoint & EncodableEndpoint, E.Input == Data
+    {
         let result = try await perform(endpoint, from: domain, data: endpoint.body)
-        return(try decode(result.0, endpoint: endpoint), result.1)
+        return (try decode(result.0, endpoint: endpoint, for: domain), result.1)
     }
 
     /// Performs a request for the specified `endpoint`
@@ -69,23 +84,24 @@ public extension EndpointService {
     ///   - body: The body data to include in the URLRequest
     /// - Returns: The data and response returned for this endpoint request
     func perform<E>(_ endpoint: E, from domain: Domain) async throws -> (E.Output, HTTPURLResponse)
-    where E: DecodableEndpoint & EncodableEndpoint, E.Input == Data, E.Output == Data {
+    where E: DecodableEndpoint & EncodableEndpoint, E.Input == Data, E.Output == Data
+    {
         try await perform(endpoint, from: domain, data: endpoint.body)
     }
-
 }
 
 // MARK: Decodable
-public extension EndpointService {
 
+public extension EndpointService {
     /// Performs a request for the specified `endpoint`
     /// - Parameters:
     ///   - endpoint: The `endpoint` configuration
     ///   - domain: The `domain` associated with this endpoint
     /// - Returns: The data and response returned for this endpoint request
     func perform<E>(_ endpoint: E, from domain: Domain) async throws -> (E.Output, HTTPURLResponse)
-    where E: DecodableEndpoint, E.Output == Data {
-        return try await perform(endpoint, from: domain, data: nil)
+    where E: DecodableEndpoint, E.Output == Data
+    {
+        try await perform(endpoint, from: domain, data: nil)
     }
 
     /// Performs a request for the specified `endpoint`
@@ -94,17 +110,17 @@ public extension EndpointService {
     ///   - domain: The `domain` associated with this endpoint
     /// - Returns: The decoded data and response returned for this endpoint request
     func perform<E>(_ endpoint: E, from domain: Domain) async throws -> (E.Output, HTTPURLResponse)
-    where E: DecodableEndpoint {
+    where E: DecodableEndpoint
+    {
         let dataEndpoint = AnyDataEndpoint(endpoint)
         let (data, response) = try await perform(dataEndpoint, from: domain, data: nil)
-        return (try decode(data, endpoint: endpoint), response)
+        return (try decode(data, endpoint: endpoint, for: domain), response)
     }
-
 }
 
 // MARK: Encodable
-public extension EndpointService {
 
+public extension EndpointService {
     /// Performs a request for the specified `endpoint`
     /// - Parameters:
     ///   - endpoint: The `endpoint` configuration
@@ -112,7 +128,8 @@ public extension EndpointService {
     ///   - body: The body data to include in the URLRequest
     /// - Returns: The data and response returned for this endpoint request
     func perform<E>(_ endpoint: E, from domain: Domain) async throws -> (Data, HTTPURLResponse)
-    where E: EncodableEndpoint, E.Input == Data {
+    where E: EncodableEndpoint, E.Input == Data
+    {
         try await perform(endpoint, from: domain, data: endpoint.body)
     }
 
@@ -123,63 +140,54 @@ public extension EndpointService {
     ///   - body: The encodable body to include in the URLRequest
     /// - Returns: The data and response returned for this endpoint request
     func perform<E>(_ endpoint: E, from domain: Domain) async throws -> (Data, HTTPURLResponse)
-    where E: EncodableEndpoint {
-        let encoded = try encode(endpoint: endpoint)
+    where E: EncodableEndpoint
+    {
+        let encoded = try encode(endpoint: endpoint, for: domain)
         return try await perform(endpoint, from: domain, data: encoded)
     }
-
 }
 
 // MARK: Private
+
 private extension EndpointService {
-    func encode<E: EncodableEndpoint>(endpoint: E) throws -> Data {
+    func encode<E: EncodableEndpoint>(endpoint: E, for domain: Domain) throws -> Data {
         do {
             let result = try endpoint.encode()
-
-#if canImport(Logging)
-            Logger.encoding.info("􀆅 \(endpoint.urlRequest) | \(E.Input.self)")
-#else
-            debugPrint("􀆅 \(endpoint.request) | \(E.Input.self)")
-#endif
-
+            observers.values.forEach {
+                $0.object?.service(self, didEncode: endpoint, for: domain)
+            }
             return result
         } catch let EncodingError.invalidValue(_, context) {
-            try throwError(error: EndpointError.encoding(E.Input.self, context), status: -1, endpoint: endpoint)
+            try throwError(error: EndpointError.encoding(E.Input.self, context), status: -1, endpoint: endpoint, for: domain)
         } catch {
-            try throwError(error: error, status: -1, endpoint: endpoint)
+            try throwError(error: error, status: -1, endpoint: endpoint, for: domain)
         }
     }
 
-    func decode<E: DecodableEndpoint>(_ data: Data, endpoint: E) throws -> E.Output {
+    func decode<E: DecodableEndpoint>(_ data: Data, endpoint: E, for domain: Domain) throws -> E.Output {
         do {
             let result = try endpoint.decode(data)
-
-#if canImport(Logging)
-            Logger.decoding.info("􀆅 \(endpoint.urlRequest) | \(E.Output.self)")
-#else
-            debugPrint("􀆅 \(endpoint.request) | \(E.Output.self)")
-#endif
+            observers.values.forEach {
+                $0.object?.service(self, didDecode: endpoint, for: domain)
+            }
             return result
         } catch let DecodingError.dataCorrupted(context) {
-            try throwError(error: EndpointError.decoding(E.Output.self, context), status: -1, endpoint: endpoint)
+            try throwError(error: EndpointError.decoding(E.Output.self, context), status: -1, endpoint: endpoint, for: domain)
         } catch let DecodingError.valueNotFound(_, context) {
-            try throwError(error: EndpointError.decoding(E.Output.self, context), status: -1, endpoint: endpoint)
+            try throwError(error: EndpointError.decoding(E.Output.self, context), status: -1, endpoint: endpoint, for: domain)
         } catch let DecodingError.typeMismatch(_, context) {
-            try throwError(error: EndpointError.decoding(E.Output.self, context), status: -1, endpoint: endpoint)
+            try throwError(error: EndpointError.decoding(E.Output.self, context), status: -1, endpoint: endpoint, for: domain)
         } catch let DecodingError.keyNotFound(_, context) {
-            try throwError(error: EndpointError.decoding(E.Output.self, context), status: -1, endpoint: endpoint)
+            try throwError(error: EndpointError.decoding(E.Output.self, context), status: -1, endpoint: endpoint, for: domain)
         } catch {
-            try throwError(error: error, status: -1, endpoint: endpoint)
+            try throwError(error: error, status: -1, endpoint: endpoint, for: domain)
         }
     }
 
-    func throwError<E: Endpoint>(error: Error, status: Int, endpoint: E) throws -> Never {
-#if canImport(Logging)
-        Logger.response.error("􀅾 \(status) | \(endpoint.method.rawValue) \(endpoint.urlRequest) | \(error.localizedDescription)")
-#else
-        debugPrint("􀅾 \(status) | \(endpoint.method.rawValue) \(endpoint.request) | \(error.localizedDescription)")
-#endif
-
+    func throwError<E: Endpoint>(error: Error, status: Int, endpoint: E, for domain: Domain) throws -> Never {
+        observers.values.forEach {
+            $0.object?.service(self, didFail: endpoint, for: domain, status: status, error: error)
+        }
         throw error
     }
 
@@ -188,29 +196,25 @@ private extension EndpointService {
         var request = try await domain.urlRequest(for: endpoint)
         request.httpBody = data
 
-#if canImport(Logging)
-        Logger.urlRequest.info("􀍠 \(endpoint.method.rawValue) \(endpoint.urlRequest)")
-#else
-        debugPrint("􀍠 \(endpoint.method.rawValue) \(endpoint.request)")
-#endif
+        observers.values.forEach {
+            $0.object?.service(self, willBegin: endpoint, for: domain)
+        }
 
         let (data, response) = try await session.data(for: request)
         guard let httpResponse = response as? HTTPURLResponse else {
-            try throwError(error: EndpointError.unexpectedResponse(response), status: -1, endpoint: endpoint)
+            try throwError(error: EndpointError.unexpectedResponse(response), status: -1, endpoint: endpoint, for: domain)
         }
 
-        guard (200..<300).contains(httpResponse.statusCode) else {
-            try throwError(error: EndpointError.badResponse(httpResponse), status: httpResponse.statusCode, endpoint: endpoint)
+        guard (200 ..< 300).contains(httpResponse.statusCode) else {
+            try throwError(error: EndpointError.badResponse(httpResponse), status: httpResponse.statusCode, endpoint: endpoint, for: domain)
         }
 
         let end = Date()
-        let interval = end.timeIntervalSinceReferenceDate - start.timeIntervalSinceReferenceDate
+        let duration = end.timeIntervalSinceReferenceDate - start.timeIntervalSinceReferenceDate
+        observers.values.forEach {
+            $0.object?.service(self, didFinish: endpoint, for: domain, duration: duration)
+        }
 
-#if canImport(Logging)
-        Logger.response.info("􀆅 \(endpoint.method.rawValue) \(endpoint.urlRequest) (\(interval)s)")
-#else
-        debugPrint("􀆅 \(endpoint.method.rawValue) \(endpoint.request) (\(interval)s)")
-#endif
         return (data, httpResponse)
     }
 }
@@ -227,7 +231,7 @@ public enum EndpointError: LocalizedError {
     case encoding(Any.Type, EncodingError.Context)
     /// The data could not be decoded
     case decoding(Any.Type, DecodingError.Context)
-    
+
     public var errorDescription: String? {
         switch self {
         case let .badEndpoint(message):
@@ -241,13 +245,13 @@ public enum EndpointError: LocalizedError {
             case 404:
                 return "Resource not found"
             default:
-                return "Bad response: \(response.statusCode)"
+                return "Bad response: \(response)"
             }
         case let .encoding(type, context):
             return "\(String(describing: type)) – \(context.debugDescription)"
         case let .decoding(type, context):
             let path = context.codingPath
-                .compactMap { $0.stringValue }
+                .map(\.stringValue)
                 .joined(separator: ".")
             return "\(String(describing: type)).\(path) – \(context.debugDescription)"
         }
